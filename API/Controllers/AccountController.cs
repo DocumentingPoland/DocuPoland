466a3c26170711d768856a886913002d9ec10445
DocumentingPoland <wtfww@protonmail.com>
2021-02-16 05:03:47 +0000

    Added register and validation at API level

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO) 
+        {
+            // check if email exists
+            if (await EmailExists(registerDTO.Email)) return BadRequest("Email aready registered");
+
+            using var hmac = new HMACSHA512();
+
+            // create user
+            var user = new AppUser() 
+            {
+                Email = registerDTO.Email.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            // track changes
+            _context.AppUsers.Add(user);
+            // save to db
+            await _context.SaveChangesAsync();
+            return user;
+            
+        }
+
+        // email exsist method
+        private async Task<bool> EmailExists(string email) 
+        {
+            return await _context.AppUsers.AnyAsync(x => x.Email == email.ToLower());
+        }
+
+    }
+}
--- API/Controllers/SubmissionsController.cs
+++ API/Controllers/SubmissionsController.cs
@@ -8,9 +8,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class SubmissionsController : ControllerBase
+
+    public class SubmissionsController : BaseApiController
     {
         private readonly DataContext _context;
         public SubmissionsController(DataContext context)
@@ -19,18 +18,18 @@ namespace API.Controllers
         }
 
         
-    [HttpGet]
-     public async Task<ActionResult<List<Submission>>> GetSubmissions() 
-    {
-        return await _context.Submissions.ToListAsync();
+        [HttpGet]
+        public async Task<ActionResult<List<Submission>>> GetSubmissions() 
+        {
+            return await _context.Submissions.ToListAsync();
 
-    }
+        }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Submission>>  GetSubmission(int id) 
-    {
-        return await _context.Submissions.FindAsync(id);
-    }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Submission>>  GetSubmission(int id) 
+        {
+            return await _context.Submissions.FindAsync(id);
+        }
 
     }
 
